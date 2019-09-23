ca9935fc043866ad45f22043378a2cc3098b9a12
mohammed <mdrame113@gmail.com>
2019-09-22 17:32:59 -0700

    hero,ability,armor,test classes added

--- Main/extension.py
+++ Main/extension.py
@@ -1,7 +1,11 @@
-#import superhero
-
-from superhero import test
 
 
-john = test("John", 20)
-john.talk()
+class man():
+    def __init__(self, name=100, age):
+        self.age = age
+        self.name = name
+
+
+
+mo = man(24,"mohammed")
+print(mo.name)
--- Main/superhero.py
+++ Main/superhero.py
@@ -1,13 +1,73 @@
+import random
 
 
-
-class test():
-    def __init__(self, name, age):
+class Hero:
+    def __init__(self, name, starting_health=100):
+        '''Instance properties:
+          abilities: List
+          armors: List
+          name: String
+          starting_health: Integer
+          current_health: Integer
+        '''
+       # TODO: Initialize instance variables values as instance variables
+       # (Some of these values are passed in above,
+       # others will need to be set at a starting value)
+       # abilities and armors are lists that will contain objects that we can use
+        self.abilities = list()
+        self.armors = list()
         self.name = name
-        self.age = age
+        self.starting_health = starting_health
+        self.current_health = starting_health
 
-    def talk(self):
-        print(f"sup with you {self.name}, I heard is you {self.age} years old")
 
-mohammed = test("Mohammed", 24)
-mohammed.talk()
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+
+    def attack(self):
+        for ability in self.abilities: #iterating over self.abilities list of object
+            if ability.attack() in ability: # checking to see is the method attack() is in ability.
+                return ability.attack() # if attack() is in return it as an intiger ‼️.
+            else:
+                return = " Sorry there is no attack method in the abilities list ""
+
+
+
+# super hero   ability
+class Ability:
+
+    def __init__(self, name, attack_strength):
+       self.name = name
+       self.attack_strength = attack_strength
+
+
+
+    def attack(self):
+      return random.randint(0,self.attack_strength)
+
+
+# super hero weapons blueprint class
+class Armor:
+    def __init__(self, name, max_block):
+        '''Instantiate instance properties.
+            name: String
+            max_block: Integer
+        '''
+        self.name = name
+        self.max_block = max_block
+        # TODO: Create instance variables for the values passed in.
+    def block(self):
+        return random.randint(0, self.max_block)
+
+
+
+#testing
+
+if __name__ == "__main__":
+
+    flying = Ability(" Flying ", 100)
+    invensibility = Ability(" Invensibility ", 99)
+    the_flask = Hero("The flask", 100)
+    the_flask.add_ability(flying)
+    the_flask.add_ability(invensibility)
+    print(the_flask.attack())
